307857b8b051fd43dfae6dab41f640520c03a3b3
kingkid159 <kingkid123@naver.com>
2025-11-13 22:38:30 +0900

    feat:db 연결 테스트

--- kiwoomweb/src/app/api/test/reoute.ts
+++ kiwoomweb/src/app/api/test/reoute.ts
@@ -6,3 +6,12 @@ export async function connectTest() {
   const data = await res.text(); // JSON 응답 파싱
   return data;
 }
+
+export async function connectDbTest() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/test/db`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" }
+  });
+  const data = await res.text(); // JSON 응답 파싱
+  return data;
+}
--- kiwoomweb/src/app/test/page.tsx
+++ kiwoomweb/src/app/test/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
 import React, { useEffect, useState } from "react";
-import { connectTest } from '@/src/app/api/test/reoute'
+import { connectTest, connectDbTest } from '@/src/app/api/test/reoute'
 
 export default function TestPage() {
   const [test, setTest] = useState('')
-
+  const [dbTest, setDbTest] = useState('')
   useEffect(() => {
-    connectTest().then((item) => { setTest(item) })
+    // connectTest().then((item) => { setTest(item) })
+    connectDbTest().then((item) => { setDbTest(item) })
   }, [])
 
 
-  return (<div>테스트 화면 : {test}</div>)
+  return (<div>테스트 화면 : {test} db : {dbTest}</div>)
 }
 
