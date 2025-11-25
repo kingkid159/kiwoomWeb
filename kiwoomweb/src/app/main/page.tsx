b43a2f67692b184eab2843645e908530b1f2bc6b
shlee-olivetech <shlee-olivetech@olivetech.co.kr>
2025-11-25 16:28:20 +0900

    feat: 그룹설정 sheet ui 개발

--- kiwoomweb/src/app/main/page.tsx
+++ kiwoomweb/src/app/main/page.tsx
@@ -1,14 +1,61 @@
 'use client'
 
-import React from 'react';
+import React,{useState}from "react"
+import BottomSheet from "@/components/common/Sheet"
+import BottomSheet2 from "@/components/common/Sheet2"
 
-const MainPage = () => {
+const Contents = ({ openSheet2 }: { openSheet2: () => void }) => {
+    return(<div className="h-full overflow-auto p-4">
+        <div className="text-center text-lg font-semibold pb-4">
+          그룹설정
+        </div>
+
+        <div className="px-6 space-y-6">
+          <div>
+            <label className="text-gray-400 text-sm">그룹명</label>
+            <input
+              type="text"
+              className="w-full border-b border-gray-200 focus:outline-none py-2"
+            />
+          </div>
+
+          <div>
+            <label className="text-gray-400 text-sm">그룹설명</label>
+            <input
+              type="text"
+              className="w-full border-b border-gray-200 focus:outline-none py-2"
+            />
+          </div>
+
+          <div className="flex items-center justify-between py-3 border-b border-gray-200" onClick={openSheet2}>
+            <span className="text-gray-900 font-medium">종목 선택</span>
+            <span className="text-gray-400">{">"}</span>
+          </div>
+        </div>
+
+        <div className="w-full py-8 flex justify-center">
+          <button className="text-blue-600 font-semibold">저장</button>
+        </div>
+        </div>)
+}
+
+const MainPage = ()=>{
+
+    const [open, setOpen] = useState(false);
+    const [open2, setOpen2] = useState(false);
 
   return (
-    <div>
-      main
-    </div>
-  );
-};
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <button
+        onClick={() => setOpen(true)}
+        className="px-4 py-2 bg-blue-500 text-white rounded-lg"
+      >
+        바텀시트 열기
+      </button>
+
+      <BottomSheet open={open} onClose={() => setOpen(false)}> <Contents openSheet2={() => setOpen2(true)} /></BottomSheet>
+      <BottomSheet2 open={open2} onClose={() => setOpen2(false)}><div>hi</div></BottomSheet2>
+    </div>)
+}
 
 export default MainPage;
---
+++ kiwoomweb/src/components/common/Sheet2.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useEffect, useRef, TouchEvent,useState, ReactElement } from "react";
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+  children:React.ReactNode;
+}
+
+export default function BottomSheet({ open, onClose, children }: Props) {
+  const sheetRef = useRef<HTMLDivElement | null>(null);
+  const startY = useRef(0);
+  const currentY = useRef(0);
+  const THRESHOLD = 120;
+ const [translateY, setTranslateY] = useState(100); 
+
+  useEffect(() => {
+    if (open) {
+      requestAnimationFrame(() => setTranslateY(0));
+    } else {
+      document.body.style.overflow = "auto";
+      setTranslateY(100);
+    }
+  }, [open]);
+
+  if (!open) return null;
+
+  const onTouchStart = (e: TouchEvent<HTMLDivElement>) => {
+    startY.current = e.touches[0].clientY;
+    currentY.current = 0;
+  };
+
+  const onTouchMove = (e: TouchEvent<HTMLDivElement>) => {
+    const y = e.touches[0].clientY;
+    const delta = y - startY.current;
+
+    if (delta > 0 && sheetRef.current) {
+      currentY.current = delta;
+      sheetRef.current.style.transform = `translateY(${delta}px)`;
+    }
+  };
+
+  const onTouchEnd = () => {
+    if (!sheetRef.current) return;
+
+    if (currentY.current > THRESHOLD) {
+      sheetRef.current.style.transition = "transform 0.25s ease-out";
+      sheetRef.current.style.transform = "translateY(100%)";
+      setTimeout(() => onClose(), 200);
+    } else {
+      sheetRef.current.style.transition = "transform 0.25s ease-out";
+      sheetRef.current.style.transform = "translateY(0px)";
+    }
+
+    setTimeout(() => {
+      if (sheetRef.current) sheetRef.current.style.transition = "";
+    }, 250);
+  };
+
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-end">
+      <div className="absolute inset-0 bg-gray-400/20 pointer-events-none"></div>
+      <div
+        ref={sheetRef}
+        className="relative w-full max-w-md mx-auto bg-white rounded-t-3xl shadow-xl pb-10 z-30 h-[88vh] overflow-hidden"
+        onTouchStart={onTouchStart}
+        onTouchMove={onTouchMove}
+        onTouchEnd={onTouchEnd}
+      >
+        <div className="w-full flex justify-center pt-4 pb-3">
+          <div className="w-16 h-1.5 bg-gray-300 rounded-full" />
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+}
